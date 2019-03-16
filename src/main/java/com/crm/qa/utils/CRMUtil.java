a3698dee893a8c86ad373dcc57a91723cd1e4705
Guddu28886 <gudduumca@gmail.com>
2019-03-17 00:54:02 +0530

    testng.xml addition to pom.xml
    testng.xml addition to pom.xml

--- src/main/java/com/crm/qa/utils/CRMUtil.java
+++ src/main/java/com/crm/qa/utils/CRMUtil.java
@@ -1,6 +1,14 @@
 package com.crm.qa.utils;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.crm.qa.base.CRMBase;
@@ -12,5 +20,23 @@ public class CRMUtil extends CRMBase{
 		driver.switchTo().defaultContent();
 		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.name("mainpanel")));
 	}
+	
+	public void drawBorder(WebElement element)
+	{
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].style.border='3px solid red'", element);
+	}
+	
+	public void highlightBackGround(WebElement element)
+	{
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].style.backGroundColor='red'", element);
+	}
 
+	public void takeScreenShot() throws IOException
+	{
+		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File(System.getProperty("user.dir")+"\\screenshots\\screenshot.png"));
+	}
+	
 }
